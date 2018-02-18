fab2db65e36a62664beb346228c26e8845836b51
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-02-18 09:11:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 
 function print_error($err){ //print to file
+	try{
+		$status = "successfully printed to file.";
 		//Set Path for file output
 		$Path =  __DIR__ .'\logs\access-error-log.txt' ; 
 		
@@ -12,13 +14,19 @@ function print_error($err){ //print to file
 		
 		//Open log file
 		$error_log = fopen($Path, "a+"); //open error log from directory
-		
+				
 		//Store new log content
 		fwrite($error_log, $err); //log error to file
 		fclose($error_log);//close file	access
+	 //catch exceptioncatch
+	}catch(Exception $e) {
+	  echo 'Error printing to file, Error Message: ' .$e->getMessage();
+	}
+	finally{
+		echo status;
+	}
 }
 
 print_error("hellol 123");
-echo "done printing.";
 
 ?>
